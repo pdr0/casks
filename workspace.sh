0a644de85e3c97feb7c1bc1977665e730e392f43
Pedro Selas <p.selas@ptytechnologies.com>
2022-05-14 17:13:02 +0200

    Remove old nodejs installation step

--- workspace.sh
+++ workspace.sh
@@ -17,10 +17,11 @@ chmod -R go-w "$(brew --prefix)/share/zsh"
 echo "Testing brew"
 brew install hello
 
+echo "Install 'n' NodeJs Version Manager"
 echo "Installing midori Browser"
 sudo apt update && sudo apt install midori
 
-echo "Download WebStorm"
+echo "Download WebStorm and decompress"
 wget "https://download-cdn.jetbrains.com/webstorm/WebStorm-2022.1.1.tar.gz"
 echo "Decompressing WebStorm"
 tar -xf WebStorm-2022.1.1.tar.gz
@@ -29,7 +30,6 @@ tar -xf WebStorm-2022.1.1.tar.gz
 Installing Docker from its own repository.
 Setting up repository
 '
-
 sudo apt-get update
 sudo apt-get install \
   ca-certificates \
@@ -38,11 +38,9 @@ sudo apt-get install \
   lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 
 :'
 Installing Docker Engine
@@ -53,17 +51,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 :'Testing installation of Docker
 Running Hello World
 '
-
 sudo docker run hello-world
 
-:'
-Install NodeJS
-Install NPM (Node Package Manager)
-Install n as nodeJs Version manager
-'
-sudo apt install nodejs
-sudo apt install npm
-npm install -g n
 
 :'
 Manual Steps:
